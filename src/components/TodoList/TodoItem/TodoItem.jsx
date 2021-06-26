f6a4cb9c930479c6a748cd8969a8de598a59edc9
EvgeniyMakaruk <makarukssd@mail.ru>
2021-06-26 11:33:48 +0300

    2

--- src/components/TodoList/TodoItem/TodoItem.jsx
+++ src/components/TodoList/TodoItem/TodoItem.jsx
@@ -1,16 +1,18 @@
 import React from 'react'
 import s from './TodoItem.module.css'
 
-function TodoItem({ todo }) {
+function TodoItem({ todo, onChange }) {
+   console.log(todo)
    return (
       <div className={s.todoItem}>
          <li>
             <span>
-               <input type="checkbox" />
-               
+               <input type="checkbox"
+                  onChange={() => onChange(todo.id)} />
+
                <strong>{todo.id + ' '}</strong>
                {todo.title}
-               </span>
+            </span>
             <button>&times;</button>
          </li>
       </div>
--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import s from './TodoList.module.css'
 import TodoItem from './TodoItem/TodoItem'
 
-function TodoList({ todos }) {
+function TodoList({ todos, onToggle }) {
+
+
    return (
       <div className={s.todoList}>
          <ul>
             {
-               todos.map(todo => <TodoItem key={todo.id}todo={todo}/>)
+               todos.map(todo => <TodoItem key={todo.id} todo={todo} onChange={onToggle} />)
             }
 
          </ul>
